ee54d14c84068fb870d626bcbe6ebcf0d78fe00b
Ragavarshithan <105372507+Ragavarshithan@users.noreply.github.com>
2023-09-11 01:11:26 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,7 @@ const Footer = () => {
     <div className="max-w-[1520px] m-auto px-4 py-2 bg-[#24262b]">
       <div className="py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300">
         <div>
-          <h1 className="w-full text-3xl font-bold text-orange-500">YumEats</h1>
+          <h1 className="w-full text-3xl font-bold text-orange-500">R's Eats</h1>
           <p>
           Welcome to r-eat, your ultimate destination for delightful dining experiences delivered right to your doorstep.
            Explore a world of culinary delights curated just for you. Order now and savor every bite! Bon appétit!
